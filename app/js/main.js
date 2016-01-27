d59a209565480c62805632f83e9feb643b811cb4
Azzazzelko <azzazzelko@gmail.com>
2016-01-27 03:34:30 +0300

    Сделан PopUp

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,26 @@
 if (!Modernizr.placeholder){
  	$('input, textarea').placeholder();
 }
+
+// ПопАп..
+
+var popUpAddButton = $("#add-site");
+var popUpAdd = $("#popUpAdd");
+var popUpSubmit = $("#popUpSubmit");
+var popUpClose = $("#popUpClose");
+
+popUpAddButton.on("click", function(e) {
+	popUpAdd.fadeTo(500, 1);
+})
+
+popUpSubmit.on("click", function(e) {
+	popUpAdd.fadeOut();
+})
+
+popUpClose.on("click", function(e) {
+	popUpAdd.fadeOut();
+	return false;
+})
+
+
+// ..END
