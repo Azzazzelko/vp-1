ab2e774847559e4e6df3c16a818fd905d922505a
Azzazzelko <azzazzelko@gmail.com>
2016-01-28 20:54:56 +0300

    Сверстаны элементы ошибок и положено начало валидации формы

--- app/js/main.js
+++ app/js/main.js
@@ -4,17 +4,12 @@ if (!Modernizr.placeholder){
 
 // ПопАп..
 
-var popUpAddButton = $("#add-site");
-var popUpAdd = $("#popUpAdd");
-var popUpSubmit = $("#popUpSubmit");
-var popUpClose = $("#popUpClose");
+var popUpAddButton = $("#add-site"),
+	popUpAdd = $("#popUpAdd"),
+	popUpClose = $("#popUpClose");
 
 popUpAddButton.on("click", function(e) {
 	popUpAdd.fadeTo(500, 1);
-})
-
-popUpSubmit.on("click", function(e) {
-	popUpAdd.fadeOut();
 })
 
 popUpClose.on("click", function(e) {
@@ -22,5 +17,65 @@ popUpClose.on("click", function(e) {
 	return false;
 })
 
-
 // ..END
+
+// Form Validation
+var fName = $("[name=projectName]"),
+	fImg = $("[name=projectImg]"),
+	fUrl = $("[name=projectURL]"),
+	fAbout = $("[name=projectAbout]"),
+	form = $("[name=addProjectForm"),
+	popUpSubmit = $("#popUpSubmit");
+
+var showError = function(NameClass) {
+    var AA = '"append"',
+		code1 = "form.append('<style class=" + AA + ">." + NameClass + ":before{display: block;}</style>')";
+		code2 = "form.append('<style class=" + AA + ">." + NameClass + ":after{display: block;}</style>')";
+	eval(code1);
+	eval(code2);
+};
+
+var hideError = function(NameClass) {
+	 var AA = '"append"',
+		code1 = "form.append('<style class=" + AA + ">." + NameClass + ":before{display: none;}</style>')";
+		code2 = "form.append('<style class=" + AA + ">." + NameClass + ":after{display: none;}</style>')";
+	eval(code1);
+	eval(code2);
+};
+
+popUpSubmit.on("click", function(e) {
+	var badForm = false;
+	if (fName.val() == "") {
+		showError("errorName");
+		fName.addClass('ErrorBorder');
+		badForm = true;
+	} else {
+		hideError("errorName");
+		fName.removeClass('ErrorBorder');
+	}
+
+	if (fUrl.val() == "") {
+		showError("errorUrl");
+		fUrl.addClass('ErrorBorder');
+		badForm = true;
+	} else {
+		hideError("errorUrl");
+		fUrl.removeClass('ErrorBorder');
+	}
+
+	if (fAbout.val() == "") {
+		showError("errorAbout");
+		fAbout.addClass('ErrorBorder');
+		badForm = true;
+	} else {
+		hideError("errorAbout");
+		fAbout.removeClass('ErrorBorder');
+	}
+
+	if (badForm) {
+		return false;
+	}
+	alert ("Форма заполнена успешно!");
+})
+
+// Validation..END
