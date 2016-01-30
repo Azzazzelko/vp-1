dd0e4e78ade03e65767ab7be605c9485471659c9
Azzazzelko <azzazzelko@gmail.com>
2016-01-30 05:55:15 +0300

    Много много JS (валидация продолжение..)

--- app/js/main.js
+++ app/js/main.js
@@ -2,125 +2,193 @@ if (!Modernizr.placeholder){
  	$('input, textarea').placeholder();
 }
 
-// ПопАп..
+var app = (function(){
+	var popUpAddButton = $("#add-site"),
+		popUpAdd = $("#popUpAdd"),
+		popUpClose = $("#popUpClose"),
+		popUpSubmit = $("#popUpSubmit"),
+		form = $("form"),
+	    fImg = $(".addProject__img"),
+		fImgName = $(".addProject__img-text"),
+		fImgInp = $("[name=img]"),
+		fName = $("[name=name]"),
+		fEmail = $("[name=email]"),
+		fCode = $("[name=code]"),
+		fUrl = $("[name=url]"),
+		fAbout = $("[name=about]"),
+		feedSubmit = $("#feedback-submit"), 
+		feedReset = $("#feedback-reset"),
+	 	items = form.find('input, textarea').not('[type="submit"], [type="file"], [type="reset"]');
 
-var popUpAddButton = $("#add-site"),
-	popUpAdd = $("#popUpAdd"),
-	popUpClose = $("#popUpClose");
+	var init = function(){
+		setUpListeners();
+	};
 
-popUpAddButton.on("click", function(e) {
-	popUpAdd.fadeTo(500, 1);
-})
+	var setUpListeners = function(){
 
-popUpClose.on("click", function(e) {
-	e.preventDefault();
-	popUpAdd.fadeOut();
-})
+		popUpAddButton.on("click", openF);
 
-// ..END
+		popUpClose.on("click", closeF);
 
-// Form Validation Add Project
+		fImgInp.on("change", function(e) {
+			imgNameToDiv(); 
+			removeErrorFromFile();
+		});
 
-// Кнопка Загрузки картинки...
-var fImg = $(".addProject__img"),
-	fImgName = $(".addProject__img-text"),
-	fImgInp = $("[name=img]");
+		items.on("keydown", hideErrorWhilePressKey);
 
-fImgInp.on("change", function(e) {
-	var indexForSlice = fImgInp.val().lastIndexOf('\\');
-	fImgName.html(fImgInp.val().substring(indexForSlice+1));
-});
-// Кнопка Загрузки картинки...END
+		popUpSubmit.on("click", formValidation);
 
-var fName = $("[name=projectName]"),
-	fUrl = $("[name=projectURL]"),
-	fAbout = $("[name=projectAbout]"),
-	form = $("[name=addProjectForm"),
-	popUpSubmit = $("#popUpSubmit");
+		feedSubmit.on("click", formValidation);
 
-var showError = function(NameClass) {
-    var AA = '"append"',
-		code1 = "form.append('<style class=" + AA + ">." + NameClass + ":before{display: block;}</style>')";
-		code2 = "form.append('<style class=" + AA + ">." + NameClass + ":after{display: block;}</style>')";
-	eval(code1);
-	eval(code2);
-};
+		feedReset.on("click", hideAllErrors);
 
-var hideError = function(NameClass) {
-	 var AA = '"append"',
-		code1 = "form.append('<style class=" + AA + ">." + NameClass + ":before{display: none;}</style>')";
-		code2 = "form.append('<style class=" + AA + ">." + NameClass + ":after{display: none;}</style>')";
-	eval(code1);
-	eval(code2);
-};
+	};
 
-popUpSubmit.on("click", function(e) {
-	var badForm = false;
-	if ($.trim(fName.val()) == "") {
-		showError("errorName");
-		fName.addClass('ErrorBorder');
-		badForm = true;
-	} else {
-		hideError("errorName");
-		fName.removeClass('ErrorBorder');
+	var closeF = function(e) {
+		e.preventDefault();
+		popUpAdd.fadeOut();
+	};
+
+	var openF = function(e) {
+		popUpAdd.fadeTo(500, 1);
+	};
+
+	var imgNameToDiv = function(e) {
+		var indexForSlice = fImgInp.val().lastIndexOf('\\');
+		fImgName.html(fImgInp.val().substring(indexForSlice+1));
 	}
 
-	if ($.trim(fUrl.val()) == "") {
-		showError("errorUrl");
-		fUrl.addClass('ErrorBorder');
-		badForm = true;
-	} else {
-		hideError("errorUrl");
-		fUrl.removeClass('ErrorBorder');
-	}
-
-	if ($.trim(fAbout.val()) == "") {
-		showError("errorAbout");
-		fAbout.addClass('ErrorBorder');
-		badForm = true;
-	} else {
-		hideError("errorAbout");
-		fAbout.removeClass('ErrorBorder');
-	}
-
-	if (fImgName.html() == "Загрузите изображение") {
-		showError("errorImg");
-		fImg.addClass('ErrorBorder');
-		badForm = true;
-	} else {
+	var removeErrorFromFile = function() {
 		hideError("errorImg");
 		fImg.removeClass('ErrorBorder');
 	}
 
-	if (badForm) {
-		return false;
-	}
-	alert ("Форма заполнена успешно!");
-})
+	var showError = function(NameClass) {
+	    var AA = '"append"',
+			code1 = "form.append('<style class=" + AA + ">." + NameClass + ":before{display: block;}</style>')";
+			code2 = "form.append('<style class=" + AA + ">." + NameClass + ":after{display: block;}</style>')";
+		eval(code1);
+		eval(code2);
+	};
 
-var items = form.find('input, textarea').not('[type="submit"], [type="file"]');
+	var hideError = function(NameClass) {
+		var AA = '"append"',
+			code1 = "form.append('<style class=" + AA + ">." + NameClass + ":before{display: none;}</style>')";
+			code2 = "form.append('<style class=" + AA + ">." + NameClass + ":after{display: none;}</style>')";
+		eval(code1);
+		eval(code2);
+	};
 
-items.on("keydown", function(e){
-	var item = e.target;
-	if (item.name == "projectURL") {
-		hideError("errorUrl");
-		fUrl.removeClass('ErrorBorder');
+	var hideErrorWhilePressKey = function(e) {
+		var item = e.target;
+		if (item.name == "url") {
+			hideError("errorUrl");
+			fUrl.removeClass('ErrorBorder');
+		}
+		if (item.name == "name") {
+			hideError("errorName");
+			fName.removeClass('ErrorBorder');
+		}
+		if (item.name == "about") {
+			hideError("errorAbout");
+			fAbout.removeClass('ErrorBorder');
+		}
+		if (item.name == "email") {
+			hideError("errorEmail");
+			fEmail.removeClass('ErrorBorder');
+		}
+		if (item.name == "code") {
+			hideError("errorCode");
+			fCode.removeClass('ErrorBorder');
+		}
 	}
-	if (item.name == "projectName") {
-		hideError("errorName");
-		fName.removeClass('ErrorBorder');
+
+	var hideAllErrors = function(e) {
+			hideError("errorUrl");
+			fUrl.removeClass('ErrorBorder');
+		
+			hideError("errorName");
+			fName.removeClass('ErrorBorder');
+		
+			hideError("errorAbout");
+			fAbout.removeClass('ErrorBorder');
+		
+			hideError("errorEmail");
+			fEmail.removeClass('ErrorBorder');
+		
+			hideError("errorCode");
+			fCode.removeClass('ErrorBorder');	
 	}
-	if (item.name == "projectAbout") {
-		hideError("errorAbout");
-		fAbout.removeClass('ErrorBorder');
+
+	var formValidation = function() {
+		var badForm = false;
+
+		if ((fEmail.length != 0) && (fEmail.val().indexOf("@") < 0)) {
+			showError("errorEmail");
+			fEmail.addClass('ErrorBorder');
+			badForm = true;
+		} else {
+			hideError("errorEmail");
+			fEmail.removeClass('ErrorBorder');
+		}
+
+		if ((fCode.length != 0) && ($.trim(fCode.val()) == "")) {
+			showError("errorCode");
+			fCode.addClass('ErrorBorder');
+			badForm = true;
+		} else {
+			hideError("errorCode");
+			fCode.removeClass('ErrorBorder');
+		}
+
+		if ((fName.length != 0) && ($.trim(fName.val()) == "")) {
+			showError("errorName");
+			fName.addClass('ErrorBorder');
+			badForm = true;
+		} else {
+			hideError("errorName");
+			fName.removeClass('ErrorBorder');
+		}
+
+		if ((fUrl.length != 0) && ($.trim(fUrl.val()) == "")) {
+			showError("errorUrl");
+			fUrl.addClass('ErrorBorder');
+			badForm = true;
+		} else {
+			hideError("errorUrl");
+			fUrl.removeClass('ErrorBorder');
+		}
+
+		if ((fAbout.length != 0) && ($.trim(fAbout.val()) == "")) {
+			showError("errorAbout");
+			fAbout.addClass('ErrorBorder');
+			badForm = true;
+		} else {
+			hideError("errorAbout");
+			fAbout.removeClass('ErrorBorder');
+		}
+
+		if ((fImgName.length != 0) && (fImgName.html() == "Загрузите изображение")) {
+			showError("errorImg");
+			fImg.addClass('ErrorBorder');
+			badForm = true;
+		} else {
+			hideError("errorImg");
+			fImg.removeClass('ErrorBorder');
+		}
+
+		if (badForm) {
+			return false;
+		}
+		
 	}
+
+	return {
+		init:init
+	}
+}());
+
+$(document).ready(function(){
+	app.init();
 });
-
-
-fImgInp.on("change", function(e) {
-	hideError("errorImg");
-	fImg.removeClass('ErrorBorder');
-})
-
-// Validation..END
-
